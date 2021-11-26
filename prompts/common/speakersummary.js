d2d2237ee08eb960186fc54f6251e0772c3109bf
DavinciDreams <64185677+DavinciDreams@users.noreply.github.com>
2021-11-26 08:06:20 -0500

    new fact summary prompt

---
+++ prompts/common/speakersummary.js
@@ -0,0 +1,10 @@
+Summarize the facts from this file: '$speaker' + facts.txt';
+{
+    "engine": "davinci-instruct-beta",
+    "prompt": "Convert my short hand into a first-hand account of the meeting:" lisaFacts.txt;
+    "temperature": 0.7,
+    "max_tokens": 64,
+    "top_p": 1.0,
+    "frequency_penalty": 0.0,
+    "presence_penalty": 0.0,
+  }
