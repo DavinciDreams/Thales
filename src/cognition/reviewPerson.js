af89241a278ef2271196676b96a143a17fa8fdf2
DavinciDreams <64185677+DavinciDreams@users.noreply.github.com>
2021-12-14 10:09:52 -0500

    Remove cruft

--- src/cognition/reviewPerson.js
+++
@@ -1,9 +0,0 @@
-{
-    "engine": "davinci-instruct-beta",
-    "prompt": "Write an employee review based on these notes:\n\nName: NameHere\nNotesHere (Example Notes:Lobster great, noisy, service polite, prices good.)\n\nReview:",
-    "temperature": 0.3,
-    "max_tokens": 64,
-    "top_p": 1,
-    "frequency_penalty": 0,
-    "presence_penalty": 0
-  }
