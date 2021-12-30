fa797c8d518d03f6165aa8871f24b6f754cc778e
DavinciDreams <lisamegawatts@gmail.com>
2021-12-30 05:12:29 -0500

    Add more logs

--- src/utilities/searchWikipedia.js
+++ src/utilities/searchWikipedia.js
@@ -1,8 +1,12 @@
 import wiki from 'wikipedia';
-import { __dirname } from "./__dirname.js";
+import {
+  __dirname
+} from "./__dirname.js";
 import weaviate from "weaviate-client";
 import fs from 'fs';
-import { makeGPTRequest } from "./makeGPTRequest.js";
+import {
+  makeGPTRequest
+} from "./makeGPTRequest.js";
 import axios from "axios";
 import path from "path";
 import glob from "glob";
@@ -26,7 +30,10 @@ export const searchWikipedia = async (keyword) => {
       "stop": ['\n']
     };
 
-    const { success, choice } = await makeGPTRequest(data, null, null, "conversation");
+    const {
+      success,
+      choice
+    } = await makeGPTRequest(data, null, null, "conversation");
     if (success) {
       keyword = choice.text;
     }
@@ -36,64 +43,65 @@ export const searchWikipedia = async (keyword) => {
   const searchResults = await wiki.search(keyword);
   console.log(searchResults);
   // If the first result contains the keyword or vice versa, probably just go with it
-  if (searchResults.results[0]?.title.toLowerCase().includes(keyword.toLowerCase()) ||
-    keyword.toLowerCase().includes(searchResults.results[0]?.title.toLowerCase())) {
+  if (searchResults.results[0] ? .title.toLowerCase().includes(keyword.toLowerCase()) ||
+    keyword.toLowerCase().includes(searchResults.results[0] ? .title.toLowerCase())) {
     keyword = searchResults.results[0].title;
-  } else
+  } else  if (searchResults.suggestion) {
+    keyword = searchResults.suggestion;
+  } else if (searchResults[0] != undefined) {
+    keyword = searchResults[0].title;
+  }
 
-    if (searchResults.suggestion) {
-      keyword = searchResults.suggestion;
-    } else if (searchResults[0] != undefined) {
-      keyword = searchResults[0].title;
-    }
-
-    console.log("Making weaviate request");
+  console.log("Making weaviate request");
   // if it's not immediately located, request from weaviate
   const res = await makeWeaviateRequest(keyword);
 
   // TODO: If certainty is below .92...
   // Fuzzy match and sort titles
- 
-    let filePath = null;
- 
-    glob(keyword + '.*', (err, files) => {
-        if (err) {
-            console.log(err);
-        } else {
-            // a list of paths to javaScript files in the current working directory
-            console.log(files);
-            filePath = files[0];
-        }
-    });
 
-if(!filePath){
-  console.log("Trying to load file");
+  let filePath = null;
+
+  glob(keyword + '.*', (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // a list of paths to javaScript files in the current working directory
+      console.log(files);
+      filePath = files[0];
+    }
+  });
+
+  if (!filePath) {
+    console.log("Trying to load file");
     try {
-  const response = await axios.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=pageimages&piprop=original&titles=${keyword}`);
-
-  if(response && response.data.query.pages.filter(page => page.original)[0]){
-    const page = response.data.query.pages.filter(page => page.original)[0];
-    const file = await axios.get(page.original.source,
-      { responseType: 'stream' });
-      
-  // store the image from the response in /images as <keyword>.jpg using fs
-  const newFilePath = path.resolve(__dirname, "images", keyword + "." + page.original.source.split('.').pop());
-  const writer = fs.createWriteStream(path.resolve(__dirname, "images", newFilePath));
-
-  file.data.pipe(writer)
-  filePath = newFilePath;
-  // {"batchcomplete":true,"query":{"pages":[{"pageid":210458,"ns":0,"title":"Waffle","original":{"source":"https://upload.wikimedia.org/wikipedia/commons/5/5b/Waffles_with_Strawberries.jpg","width":2592,"height":1944}}]}}
+      const response = await axios.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=pageimages&piprop=original&titles=${keyword}`);
+      console.log("Response is", response)
+      if (response && response.data.query.pages.filter(page => page.original)[0]) {
+        const page = response.data.query.pages.filter(page => page.original)[0];
+        console.log("Getting file");
+        const file = await axios.get(page.original.source, {
+          responseType: 'stream'
+        });
+        console.log("file is", file)
+        // store the image from the response in /images as <keyword>.jpg using fs
+        const newFilePath = path.resolve(__dirname, "images", keyword + "." + page.original.source.split('.').pop());
+        console.log("New file path is", newFilePath);
+        const writer = fs.createWriteStream(path.resolve(__dirname, "images", newFilePath));
+        console.log("Created writer");
+        file.data.pipe(writer)
+        filePath = newFilePath;
+        // {"batchcomplete":true,"query":{"pages":[{"pageid":210458,"ns":0,"title":"Waffle","original":{"source":"https://upload.wikimedia.org/wikipedia/commons/5/5b/Waffles_with_Strawberries.jpg","width":2592,"height":1944}}]}}
+      }
+    } catch (error) {
+      console.log("Error is " + error);
+    }
   }
-} catch (error){
-  console.log ("Error is " + error);
-}
-}
   // Get wikipedia article for first result and cache
 
-// clubhouse?
+  // clubhouse?
 
 
-// TODO: Check if we already have the image for the keyword before doing all that hard stuff
+  // TODO: Check if we already have the image for the keyword before doing all that hard stuff
   // Return object containing hasImage: true and the file URI
   // Change the places that call this function to reflect payload
   // Handle sending image with response to this initialization
@@ -101,7 +109,10 @@ if(!filePath){
   // Only load or send image for platforms where it matters
   console.log("Looking up result on wikipedia");
   const result = await lookUpOnWikipedia(res.Paragraph[0].inArticle[0].title);
-  return { result, filePath}
+  return {
+    result,
+    filePath
+  }
 }
 
 export const makeWeaviateRequest = async (keyword) => {
@@ -134,7 +145,12 @@ export async function lookUpOnWikipedia(subject) {
     }
 
     // if it doesn't, fetch it from wikipedia and save it to the file
-    const { title, displaytitle, description, extract } = await wiki.summary(subject);
+    const {
+      title,
+      displaytitle,
+      description,
+      extract
+    } = await wiki.summary(subject);
     const summary = {
       title,
       displaytitle,
@@ -144,7 +160,9 @@ export async function lookUpOnWikipedia(subject) {
     // create a directory recursively at data/wikipedia/ if it doesn't exist
     const dir = __dirname + "/data/wikipedia/";
     if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir, { recursive: true });
+      fs.mkdirSync(dir, {
+        recursive: true
+      });
     }
 
     fs.writeFileSync(fileName, JSON.stringify(summary));
